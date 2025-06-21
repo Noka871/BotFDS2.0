7f684a3ea5c3d6ed4e4632e15865d292c045760b
Noka871 <58460996+Noka871@users.noreply.github.com>
2025-06-21 07:15:56 +0700

    Add files via upload

--- BotFDS2.0/dubbing_bot/services/broadcaster.py
+++ BotFDS2.0/dubbing_bot/services/broadcaster.py
@@ -1,21 +1,24 @@
 # services/broadcaster.py
+import logging
+
 from aiogram import Bot
 from database.requests import get_active_users
-
 async def broadcast(bot: Bot, text: str):
     users = await get_active_users()
     for user in users:
         await bot.send_message(user.telegram_id, text)
 
 from aiogram import Bot
-from database.requests import get_active_users
 
 async def broadcast_message(bot: Bot, text: str):
     """Массовая рассылка сообщения всем пользователям"""
     users = await get_active_users()
     for user in users:
         try:
-            await bot.send_message(chat_id=user.telegram_id, text=text)
+            await bot.send_message(user.telegram_id, text)
+            except Exception as e:
+            logging.error(f"Ошибка отправки {user.telegram_id}: {e}")
+
         except Exception as e:
             print(f"Ошибка отправки пользователю {user.telegram_id}: {e}")
 
