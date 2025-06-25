401b5eb8709e278c2132aa38f3195052637386e9
Noka871 <58460996+Noka871@users.noreply.github.com>
2025-06-25 18:52:12 +0700

    Delete BotFDS2.0 directory

--- BotFDS2.0/bot/handlers/common/__init__.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Router
-from .start import start_router
-
-common_router = Router()
-common_router.include_router(start_router)
--- BotFDS2.0/bot/states/dubber.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class ReportStates(StatesGroup):
-    select_title = State()
-    select_episode = State()
-    report_status = State()
-    delay_reason = State()
